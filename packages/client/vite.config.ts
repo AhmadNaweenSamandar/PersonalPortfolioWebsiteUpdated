3ab12cd1d73fd281697840b96c8c99c613c4347a
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-01-07 21:21:40 -0500

    Add: get route in server, useState in app and proxy in frontend configuration

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    /// This 'proxy' setting acts as a middleman during development.
+    proxy: {
+      // It creates a rule: "Look for any request starting with '/api'"
+      // When a match is found, forward that request to this target (your backend).
+        // Note: Use 'http' if your backend is not using SSL (https). 
+        // Most local Node servers run on 'http'.
+      '/api': 'http://localhost:5000'
+    }
+  }
 })
