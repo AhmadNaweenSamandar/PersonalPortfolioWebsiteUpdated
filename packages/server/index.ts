e5b6b95372fb0be42a076c36c77b86c04a885345
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-01-09 20:58:33 -0500

    Gemini API integrated with endpoint calls

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,56 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { GoogleGenAI } from '@google/genai';
+
 
 dotenv.config();
+
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
+
+// accessing the key in a variable
+const apiKey = process.env.GEMINI_API_KEY;
+
+//handling error if the key is missing
+if(!apiKey){
+   console.error("ERROR: GEMINI_API_KEY is not set in .env file");
+   process.exit(1);//stopping the server
+}
+
+// Initialize the API client with key
+const genAI = new GoogleGenAI({apiKey: apiKey});
+
+//endpoint for receiving data from clients
+app.post('/api/chat', async (req: Request, res: Response): Promise<any> => {
+    try {
+        const { prompt } = req.body;
+
+        if (!prompt) {
+            return res.status(400).json({ error: "Prompt is required" });
+        }
+
+        
+        
+        // 4. Await the response and extract text using the helper method
+        // 3. Call generateContent on the model instance
+        const response = await genAI.models.generateContent({
+         model: "gemini-3-flash-preview",
+         contents: prompt,
+        })
+
+        //accessing the text
+        const text = response.text;
+
+        res.json({ message: text });
+
+    } catch (error) {
+        console.error('GenAI Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello World!');
@@ -21,6 +67,9 @@ app.get('/api/hello', (req: Request, res: Response) => {
    // 3. Sends the response and closes the connection.
    res.json({ message: 'Hello World!' });
 });
+
+
+
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
