1a647f5d302870fe5eeb00fcc399dfaa767e7b81
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-01-12 20:16:02 -0500

    Frontend connected with backend with cors library script

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,13 +2,22 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { chatController } from './controller/chat.controller';
-
+import cors from 'cors';
 
 
 dotenv.config();
 
 
 const app = express();
+
+// 1. CORS Allow Frontend Connection
+app.use(cors({
+  origin: "http://localhost:5173", // front end server link
+  methods: ["POST", "GET"],
+  allowedHeaders: ["Content-Type"]
+}));
+
+
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
