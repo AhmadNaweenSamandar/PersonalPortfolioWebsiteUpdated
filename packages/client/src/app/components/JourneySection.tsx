6b8cc048032ef1174d22ad00480eaedd7f47ec72
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-01-15 22:47:06 -0500

    Add: Journey items interface and function

---
+++ packages/client/src/app/components/JourneySection.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+interface JourneyItem {
+   date: string;
+   title: string;
+   description: string;
+   startDate: string;
+   endDate: string;
+   logo: string;
+   teamPhoto: string;
+}
+
+export function JourneySection() {}
