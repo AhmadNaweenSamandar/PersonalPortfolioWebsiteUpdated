2661b3f821866c9fd0a67fc25382ab502a1106d0
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-01-17 19:22:07 -0500

    Add: event details, ready to use in event container section

--- packages/client/src/app/components/EventsSection.tsx
+++ packages/client/src/app/components/EventsSection.tsx
@@ -17,4 +17,42 @@ interface Event {
    photos: EventPhoto[];
 }
 
-export function EventsSection() {}
+export function EventsSection() {
+   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
+   const [selectedPhoto, setSelectedPhoto] = useState<EventPhoto | null>(null);
+
+   const events: Event[] = [
+      {
+         id: 1,
+         title: 'Zwanan Team Introduction',
+         date: 'Jun 2025',
+         image: '',
+         description: 'Cricket team introduction and youth appreciation event.',
+         photos: [
+            { id: 1, url: '', caption: 'Opening Ceremony' },
+            { id: 2, url: '', caption: 'Speaker' },
+            { id: 3, url: '', caption: 'Presentation' },
+            { id: 4, url: '', caption: 'Team introduction' },
+            { id: 5, url: '', caption: 'Networking' },
+            { id: 6, url: '', caption: 'Snacks' },
+         ],
+      },
+
+      {
+         id: 2,
+         title: 'Workshop regarding Artificial Intelligence',
+         date: 'August 2025',
+         image: '',
+         description:
+            'Hands on workshop about Large-Language-Models, how to use ChatGPT and Gemini along side learning opportunities in AI discussed.',
+         photos: [
+            { id: 1, url: '', caption: 'Team Working' },
+            { id: 2, url: '', caption: 'Presentation' },
+            { id: 3, url: '', caption: 'Team picture' },
+            { id: 4, url: '', caption: 'Networking' },
+            { id: 5, url: '', caption: '' },
+            { id: 6, url: '', caption: '' },
+         ],
+      },
+   ];
+}
